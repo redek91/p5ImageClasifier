bbf5b38cd1606273522f848632a174e37d646bc4
Manuel Roat <roat.manuel@gmail.com>
2021-12-04 17:36:42 +0100

    first commit

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,42 @@
+let mobilenet;
+let puffin;
+
+function setup() {
+  createCanvas(900,600);
+  puffin = createImg('images/puffin.jpg', imageReady);
+  puffin.hide();
+  background(0);
+
+  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+}
+
+function imageReady(){
+  image(puffin, 0, 0, width, height);
+}
+
+function modelReady(){
+  console.log('Model is ready!');
+
+  mobilenet.predict(puffin, gotResults);
+}
+
+function gotResults(error, results){
+  if(error){
+    console.error(error);
+  }else{
+    console.log(results);
+
+    let label = results[0].label;
+    let prob = results[0].confidence;
+    fill(0);
+    textSize(64);
+    text(label, 10,height - 100);
+    createP(label);
+    createP(prob);
+
+  }
+}
+
+function draw() {
+  
+}
